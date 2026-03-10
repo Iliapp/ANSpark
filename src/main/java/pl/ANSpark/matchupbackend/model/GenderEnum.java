484d6c418046e0d60b84da8f1940b2ec4acadfc6
Ilya Smakovskyi <iluhasmakovskyi@gmail.com>
2026-03-10 18:02:28 +0100

    feature/add/docker/update-ApplicationProperties/pom-xml/2-enum(classes)/GenderEnum/PreferenceEnum

---
+++ src/main/java/pl/ANSpark/matchupbackend/model/GenderEnum.java
@@ -0,0 +1,6 @@
+package pl.ANSpark.matchupbackend.model;
+
+public enum GenderEnum {
+    MALE,FEMALE,OTHER
+
+}
---
+++ src/main/java/pl/ANSpark/matchupbackend/model/PreferenceEnum.java
@@ -0,0 +1,5 @@
+package pl.ANSpark.matchupbackend.model;
+
+public enum PreferenceEnum {
+    MEN,WOMEN,BOTH
+}
