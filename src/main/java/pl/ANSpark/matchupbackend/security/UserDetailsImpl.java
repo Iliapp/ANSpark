eb5e598a791e54300c86894b498da23c5b2bb0bf
Ilya Smakovskyi <iluhasmakovskyi@gmail.com>
2026-03-13 17:32:23 +0100

    feature/add/service/jwt

--- src/main/java/pl/ANSpark/matchupbackend/security/UserDetailsImpl.java
+++ src/main/java/pl/ANSpark/matchupbackend/security/UserDetailsImpl.java
@@ -1,4 +1,75 @@
 package pl.ANSpark.matchupbackend.security;
 
-public class UserDetailsImpl {
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import pl.ANSpark.matchupbackend.model.User;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UserDetailsImpl implements UserDetails {
+    public UUID id;
+    public String email;
+    public String passwordHash;
+
+    public UserDetailsImpl(UUID id, String email, String passwordHash) {
+        this.id = id;
+        this.email = email;
+        this.passwordHash = passwordHash;
+    }
+
+    public static UserDetailsImpl build(User user) {
+        return new UserDetailsImpl(
+                user.getId(),
+                user.getEmail(),
+                user.getPasswordHash()
+        );
+    }
+
+
+
+
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return passwordHash;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
+
